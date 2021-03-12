065f6bbbb238c9450532d090aab15d0f8063fcc2
Rafael A <58530588+RafaAbranttes@users.noreply.github.com>
2021-03-12 04:35:43 -0300

    Delete ApiKey.js

--- src/ApiKey.js
+++
@@ -1,8 +0,0 @@
-
-export function RetornarKey(){
-    return 'b9167a345e8eadd3071280ef9c326cce' //adicionar a key
-}
-
-export function RetornarKeyBase(){
-    return 'https://api.themoviedb.org/3'   //adicionar a url padrão da api
-}
