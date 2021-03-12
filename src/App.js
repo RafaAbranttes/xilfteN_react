c6e103a16d71ebfc6cb8d4352025a37828a8b581
Rafael Abrantes <rafael.vieira.abranttes@gmail.com>
2021-03-12 04:31:21 -0300

    listagem pre-pronta

--- src/App.js
+++ src/App.js
@@ -1,10 +1,44 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import Tmdb from './TmDb'
+import './App.css'
+import MovieRow from './components/MovieRow'
+import FeaturedMovie from './components/FeaturedMovie'
 
 // eslint-disable-next-line import/no-anonymous-default-export
-export default () =>{
+export default () => {
+
+  const [movieList, setMovieList] = useState([])
+  const [featureData, serFeatureData] = useState(null)
+
+  useEffect(() => {
+    const loadAll = async () => {
+      let list = await Tmdb.getHomeList();
+      setMovieList(list)
+
+      let originals = list.filter(i => i.slug === 'originals')
+      let randomChosen = Math.floor(Math.random() * (originals[0].items.results.length - 1))
+      let chosen = originals[0].items.results[randomChosen]
+
+      console.log(chosen)
+    }
+
+    loadAll()
+  }, []);
+
   return (
-    <div>
-      Olá mundo!
+    <div className="page">
+
+      {featureData && 
+        <FeaturedMovie item = {featureData}></FeaturedMovie>
+      }
+
+      <section className="lists">
+        {movieList.map((item, key) => (
+          <MovieRow key={key} title={item.title} items={item.items}>
+
+          </MovieRow>
+        ))}
+      </section>
     </div>
   )
 }
--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import TmDb from './TmDb'
-import APIKEY from './api_key'
 
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
-    <APIKEY></APIKEY>
   </React.StrictMode>,
   document.getElementById('root')
 );
